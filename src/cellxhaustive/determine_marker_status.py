805b5742d6c48fca7454f46dbf6e524a524932ff
aathbt <antonin.thiebaut57@gmail.com>
2024-01-05 17:27:27 +0100

    Updates variable names to fit check_all_subsets.py

--- src/cellxhaustive/determine_marker_status.py
+++ src/cellxhaustive/determine_marker_status.py
@@ -10,8 +10,8 @@ import numpy as np
 
 
 # Function used in marker_combinations_scoring.py
-def determine_marker_status(mat_comb, markers_comb, three_peak_markers=[],
-                            two_peak_threshold=3,
+def determine_marker_status(mat_comb, markers_comb, two_peak_threshold=3,
+                            three_peak_markers=[],
                             three_peak_lower_threshold=2,
                             three_peak_upper_threshold=4):
     """
@@ -31,14 +31,14 @@ def determine_marker_status(mat_comb, markers_comb, three_peak_markers=[],
     markers_comb: array(str)
       1-D numpy array with markers matching each column of 'mat_comb'.
 
-    three_peak_markers: list(str) (default=[])
-      List of markers that have three peaks.
-
     two_peak_threshold: float (default=3)
       Threshold to consider when determining whether a two-peaks marker is
       negative or positive. Expression below this threshold means the marker
       will be considered negative. Conversely, expression above this threshold
       means the marker will be considered positive.
+
+    three_peak_markers: list(str) (default=[])
+      List of markers that have three peaks.
 
     three_peak_lower_threshold: float (default=2)
       Threshold to consider when determining whether a three-peaks marker is
@@ -54,20 +54,20 @@ def determine_marker_status(mat_comb, markers_comb, three_peak_markers=[],
 
     Returns:
     --------
-    cell_types: array(str)
+    phenotypes: array(str)
       1-D numpy array of strings matching order of cells in 'mat_comb'. Each
       string contains the cell marker types ('+', '-', 'low'), with markers
       order matching the one of 'markers_comb'.
     """
 
     # Create an array containing empty strings to store cell types
-    cell_types = np.empty(mat_comb.shape[0], dtype=np.dtype('U100'))
+    phenotypes = np.empty(mat_comb.shape[0], dtype=np.dtype('U100'))
 
     # Create a special array iterator
     iterator = np.nditer(mat_comb, flags=['multi_index'], order='C')
 
-    # Iterate over expression matrix with the 'multi_index' flag to retrieve
-    # current position in array
+    # Iterate over expression matrix with 'multi_index' flag to retrieve current
+    # position in array
     for expression in iterator:
 
         # Extract row and col indices
@@ -90,9 +90,9 @@ def determine_marker_status(mat_comb, markers_comb, three_peak_markers=[],
 
         # Append current marker type to string of marker types of corresponding
         # cell. Separate markers with a '/' if required
-        if not cell_types[cell]:
-            cell_types[cell] += f'{markers_comb[marker_pos]}{marker_type}'
-        else:
-            cell_types[cell] += f'/{markers_comb[marker_pos]}{marker_type}'
+        if not phenotypes[cell]:  # String is empty
+            phenotypes[cell] += f'{markers_comb[marker_pos]}{marker_type}'
+        else:  # String is not empty, so we separate markers with '/'
+            phenotypes[cell] += f'/{markers_comb[marker_pos]}{marker_type}'
 
-    return cell_types
+    return phenotypes
--- src/cellxhaustive/marker_combinations_scoring.py
+++ src/cellxhaustive/marker_combinations_scoring.py
@@ -18,9 +18,9 @@ from determine_marker_status import determine_marker_status  # AT. Double-check 
 # AT. Improve description
 def marker_combinations_scoring(mat_comb, markers_comb,
                                 batches_label, samples_label,
+                                three_peak_markers=[],
                                 x_samplesxbatch_space=np.round(np.arange(0.5, 1.01, 0.1), 1),
-                                y_cellxsample_space=np.arange(10, 101, 10),
-                                three_peak_markers=[]):
+                                y_cellxsample_space=np.arange(10, 101, 10)):
     """
     Cell line subdivision.
     # AT. Add function description (use the one before?)
@@ -43,6 +43,9 @@ def marker_combinations_scoring(mat_comb, markers_comb,
     samples_label: array(str)
       1-D numpy array with sample names of each cell of 'mat_comb'.
 
+    three_peak_markers: list(str) (default=[])
+      List of markers that have three peaks.
+
     x_samplesxbatch_space: array(float) (default=[0.5, 0.6, 0.7, ..., 1])
       Minimum proportion of samples within each batch with at least
       'y_cellxsample_space' cells for a new annotation to be considered. In other
@@ -57,16 +60,13 @@ def marker_combinations_scoring(mat_comb, markers_comb,
       100 cells/sample in at least 50%, 60%... 100% of samples (see description
       of previous parameter) within a batch to be considered.
 
-    three_peak_markers: list(str) (default=[])
-      List of markers that have three peaks.
-
     Returns:
     --------
       # AT. Add what is returned by the function
     """
 
     # Determine markers status of 'markers_comb' using expression data
-    cell_types = determine_marker_status(mat_comb=mat_comb,
+    phenotypes = determine_marker_status(mat_comb=mat_comb,
                                          markers_comb=markers_comb,
                                          three_peak_markers=three_peak_markers,
                                          two_peak_threshold=3,
@@ -75,82 +75,82 @@ def marker_combinations_scoring(mat_comb, markers_comb,
     # AT. Could return an array of 'cell' rows * 'markers_comb' columns if it's easier
     # AT. Or an array of array instead of an array of lists?
 
-    # Initialize matrices to store results
-    nb_cell_types = np.zeros((len(x_samplesxbatch_space), len(y_cellxsample_space)))
+    # Initialize arrays to store results
+    nb_phenotypes = np.zeros((len(x_samplesxbatch_space), len(y_cellxsample_space)))
     nb_undefined_cells = np.zeros((len(x_samplesxbatch_space), len(y_cellxsample_space)))
 
-    # Process marker status combinations returned by previous function
-    # 'determine_marker_status()' and check whether they are worth keeping
-    for cell_type in np.unique(cell_types):
+    # Process marker status combinations returned by 'determine_marker_status()'
+    # and check whether they are worth keeping
+    for phenotype in np.unique(phenotypes):
         # AT. Multithread/process here? Conflict between batches?
 
-        ###### AT. Discuss with Bernat about the importance of 'cell_type' presence in all batches (np.logical_and vs np.logical_or)
-        # Initialise temporary array to store 'cell_type' results
-        keep_cell_type = np.full(np.shape(nb_cell_types), False)
+        ###### AT. Discuss with Bernat about the importance of 'phenotype' presence in all batches (np.logical_and vs np.logical_or)
+        # Initialise temporary array to store 'phenotype' results
+        keep_phenotype = np.full(np.shape(nb_phenotypes), False)
 
         ##### AT. Former code
-        # # Initialise temporary array to store 'cell_type' results
-        # keep_cell_type = np.full(np.shape(nb_cell_types), True)
+        # # Initialise temporary array to store 'phenotype' results
+        # keep_phenotype = np.full(np.shape(nb_phenotypes), True)
 
         # Process batches separately
         for batch in np.unique(batches_label):
             # AT. Multithread/process here?
 
             # Split cell type data according to batch
-            cell_types_batch = cell_types[batches_label == batch]
+            phenotypes_batch = phenotypes[batches_label == batch]
 
             # Split sample data, first according to batch and then cell type
-            cell_type_samples = samples_label[batches_label == batch][cell_types_batch == cell_type]
+            phenotype_samples = samples_label[batches_label == batch][phenotypes_batch == phenotype]
 
-            # If there are no cells of type 'cell_type' in 'batch', skip
-            if cell_type_samples.size == 0:
+            # If there are no cells of type 'phenotype' in 'batch', skip
+            if phenotype_samples.size == 0:
                 continue
 
             # Calculate number of different samples in current batch and cell type
-            samples_nb = float(len(np.unique(cell_type_samples)))
+            samples_nb = float(len(np.unique(phenotype_samples)))
 
-            # Count number of cells with 'cell_type' marker combination in each sample
-            cell_count_sample = np.asarray([np.sum(cell_type_samples == smpl)
-                                            for smpl in np.unique(cell_type_samples)])
+            # Count number of cells with 'phenotype' marker combination in each sample
+            cell_count_sample = np.asarray([np.sum(phenotype_samples == smpl)
+                                            for smpl in np.unique(phenotype_samples)])
 
             # Check whether cell counts satisfy y threshold
-            keep_cell_type_batch = cell_count_sample[:, np.newaxis] >= y_cellxsample_space
+            keep_phenotype_batch = cell_count_sample[:, np.newaxis] >= y_cellxsample_space
             # Note: we use np.newaxis to add a dimension to work on different
             # samples concurrently
 
             # Calculate proportion of samples in current batch satisfying
             # 'y_cellxsample_space' condition
-            keep_cell_type_batch = (np.sum(keep_cell_type_batch, axis=0) / samples_nb)
+            keep_phenotype_batch = (np.sum(keep_phenotype_batch, axis=0) / samples_nb)
             # Notes:
-            # - keep_cell_type_batch is a boolean array, so we can calculate its sum
-            # - np.sum(keep_cell_type_batch, axis=0) calculates the number of samples
+            # - keep_phenotype_batch is a boolean array, so we can calculate its sum
+            # - np.sum(keep_phenotype_batch, axis=0) calculates the number of samples
             # satisfying y condition for a given y in the grid
 
             # Check whether sample proportions satisfy x threshold
-            keep_cell_type_batch = keep_cell_type_batch >= x_samplesxbatch_space[:, np.newaxis]
+            keep_phenotype_batch = keep_phenotype_batch >= x_samplesxbatch_space[:, np.newaxis]
             # Note: [:, np.newaxis] is used to transpose the 1-D array into a 2D
             # array to allow comparison
 
-            ###### AT. Discuss with Bernat about the importance of 'cell_type' presence in all batches (np.logical_and vs np.logical_or)
-            # Add nummber of undefined cells to counter
-            nb_undefined_cells += (keep_cell_type_batch == False) * np.sum(cell_count_sample)
+            ###### AT. Discuss with Bernat about the importance of 'phenotype' presence in all batches (np.logical_and vs np.logical_or)
+            # Add number of undefined cells to counter
+            nb_undefined_cells += (keep_phenotype_batch == False) * np.sum(cell_count_sample)
 
             # Intersect batch results with general results
-            keep_cell_type = np.logical_or(keep_cell_type, keep_cell_type_batch)
+            keep_phenotype = np.logical_or(keep_phenotype, keep_phenotype_batch)
 
-        # Add cell_type presence to cell type counter
-        nb_cell_types += keep_cell_type * 1
+        # Add 'phenotype' presence/absence to cell type counter
+        nb_phenotypes += keep_phenotype * 1
 
-    return nb_cell_types, nb_undefined_cells
+    return nb_phenotypes, nb_undefined_cells
 
     #         ###### AT. Former code
     #         # Intersect batch results with general results
-    #         keep_cell_type = np.logical_and(keep_cell_type, keep_cell_type_batch)
+    #         keep_phenotype = np.logical_and(keep_phenotype, keep_phenotype_batch)
 
-    #     nb_cell_types += keep_cell_type * 1
-    #     nb_undefined_cells += (keep_cell_type == False) * np.sum(cell_count_sample)
+    #     nb_phenotypes += keep_phenotype * 1
+    #     nb_undefined_cells += (keep_phenotype == False) * np.sum(cell_count_sample)  # AT. Rework this to count 'phenotype' cells in all batches
 
-    # return nb_cell_types, nb_undefined_cells
+    # return nb_phenotypes, nb_undefined_cells
 
 
 
