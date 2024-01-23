5ef1c380d596a0553c7d4ec4760d86d1532ecf1e
aathbt <antonin.thiebaut57@gmail.com>
2024-01-23 09:05:26 +0100

    Moves cellxhaustive() content in ifmain

--- src/cellxhaustive/cellxhaustive.py
+++ src/cellxhaustive/cellxhaustive.py
@@ -1,5 +1,6 @@
 """
-AT. Improve general description.
+# AT. Improve general description.
+# AT. Add what is returned by the script
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  Utility functions to run pipeline for phenotype identification
@@ -14,6 +15,11 @@ NOTES:
 Here I am sticking with the clean version of it, but I did try\
 several versions of the same pipeline (see github history)
 --------------------------------------------------------------------
+
+Pipeline for automated gating, feature selection, and clustering to
+generate new annotations.
+# AT. Update description
+
 """
 
 
@@ -31,73 +37,6 @@ from identify_phenotypes import identify_phenotypes
 # from cellxhaustive.identify_phenotypes import identify_phenotypes  # AT. Double-check path
 
 
-# AT. Add description
-def cellxhaustive(mat, markers, batches, samples, cell_labels,
-                  max_markers=15, min_annotations=3,
-                  min_cellxsample=10, min_samplesxbatch=0.5,
-                  knn_refine=True, knn_min_probability=0.5):
-    # AT. Missing three_peak_markers to carry over in all functions?
-    # AT. Same for peak thresholds
-    # AT. ==> Discuss this with Bernat
-    """
-    Pipeline for automated gating, feature selection, and clustering to
-    generate new annotations.
-    # AT. Update description
-
-    Parameters
-    ----------
-    mat: array(float)
-      2-D numpy array expression matrix, with cells in D0 and markers in D1.
-      In other words, rows contain cells and columns contain markers.
-
-    markers: array(str)
-      1-D numpy array with markers matching each column of 'mat'.
-
-    batches: array(str)
-      1-D numpy array with batch names of each cell of 'mat'.
-
-    samples: array(str)
-      1-D numpy array with sample names of each cell of 'mat'.
-
-    cell_labels: array(str)
-      1-D numpy array with main cell labels of each cell of 'mat'.
-      # AT. Set as an option, as it was used for the main gating
-      # AT. Problem with annotations ontology/vocabulary?
-
-    max_markers: int (default=15)
-      Maximum number of relevant markers to select among total list of markers
-      from total markers array. Must be less than or equal to 'len(markers)'.
-
-    min_annotations: int (default=3)
-      Minimum number of phenotypes for a combination of markers to be taken into
-      account as a potential cell population. Must be in '[2; len(markers)]',
-      but it is advised to choose a value in '[3; len(markers) - 1]'.
-
-    min_samplesxbatch: float (default=0.5)
-      Minimum proportion of samples within each batch with at least 'min_cellxsample'
-      cells for a new annotation to be considered. In other words, by default, an
-      annotation needs to be assigned to at least 10 cells/sample (see description
-      of next parameter) in at least 50% of samples within a batch to be considered.
-
-    min_cellxsample: float (default=10)
-      Minimum number of cells within each sample in 'min_samplesxbatch' % of samples
-      within each batch for a new annotation to be considered. In other words, by
-      default, an annotation needs to be assigned to at least 10 cells/sample in at
-      least 50% of samples (see description of previous parameter) within a batch
-      to be considered.
-
-    knn_refine: bool (default=True)
-      If True, clustering done via permutations of relevant markers will be refined
-      using a KNN classifier.
-
-    knn_min_probability: float (default=0.5)
-      Confidence threshold for KNN classifier to reassign a new cell type
-      to previously undefined cells.
-
-    Returns:
-    --------
-      # AT. Add what is returned by the function
-    """
 # Parse arguments
 parser = argparse.ArgumentParser(description='Script to annotate cell types using \
                                 CITE-seq ADT data.')
@@ -151,38 +90,86 @@ parser.add_argument('-knn', '--knn-min-probability', dest='knn_min_probability',
 args = parser.parse_args()
 
 
-    annotations = np.asarray(['undefined'] * len(cell_labels)).astype('U100')
+# Main script execution
+if __name__ == '__main__':
 
-    # AT. Import cell_ontology here? To import it once instead of several times in
-    # scripts further down in the workflow?
-    # AT. Make this path more flexible, maybe with a default value in argument parsing?
+    # Get 1-D array for markers
+    markers = pd.read_csv(args.marker_path, sep='\t', header=None).to_numpy().flatten()  # AT. Delete in prod
+    # markers = pd.read_csv('data/markers.txt', sep='\t', header=None).to_numpy().flatten()  # AT. Delete in prod
+
+    # Parse general input files into several arrays
+    input_table = pd.read_csv(args.input_path, sep='\t', header=0, index_col=0)  # AT. Delete in prod
+    # input_table = pd.read_csv('data/complete_table.tsv', sep='\t', header=0)  # AT. Delete in prod
+
+    # Get 2-D array for expression using 'markers'
+    mat = input_table.loc[: , markers].to_numpy()
+
+    # Get 1-D array for batch; add common batch value if information is missing
+    if 'batch' in input_table.columns:
+        batches = input_table.loc[: , 'batch'].to_numpy()
+    else:
+        batches = np.full(input_table.shape[0], 'batch0')
+
+    # Get 1-D array for sample; add common sample value if information is missing
+    if 'sample' in input_table.columns:
+        samples = input_table.loc[: , 'sample'].to_numpy()
+    else:
+        samples = np.full(input_table.shape[0], 'sample0')
+
+    # Get 1-D array for pre-annotated cell type
+    cell_labels = input_table.loc[: , 'cell_type'].to_numpy()
+
+    # Get three peaks markers if a file is specified, otherwise use default list
+    three_path = args.three_peak_markers
+    if (not isinstance(three_path, list) and pathlib.Path(three_path).is_file()):
+        with open(three_path) as file:
+            lines = file.read().splitlines()
+    else:
+        three_peak_markers = args.three_peak_markers
+
+    # Import cell types definitions
+    with open(args.cell_type_path) as in_cell_types:  # AT. Delete in prod
+    # with open('../data/config/major_cell_types.json') as in_cell_types:  # AT. Delete in prod
+        cell_types_dict = json.load(in_cell_types)
     # Note: this file was created using data from
     # https://github.com/RGLab/rcellontologymapping/blob/main/src/src/ImmportDefinitions.hs
-    with open('../data/config/major_cell_types.json') as in_cell_types:
-        cell_types_dict = json.load(in_cell_types)
 
+    # Get other parameter values from argument parsing
+    max_markers = args.max_markers
+    min_annotations = args.min_annotations
+    min_cellxsample = args.min_cellxsample
+    min_samplesxbatch = args.min_samplesxbatch
+    knn_refine = args.knn_refine
+    knn_min_probability = args.knn_min_probability
+
+    # Initialise empty array to store new annotations
+    annotations = np.asarray(['undefined'] * len(cell_labels)).astype('U100')
+
+# AT. Double-check what follows
+
+    # Process cells by pre-annotations
     for label in np.unique(cell_labels):
 
         # Create boolean array to select cells matching current label
         is_label = (cell_labels == label)
 
-        # AT. Add annotations on what function does
+        # Get annotations for all cells of type 'label'
         cell_groups_name, clustering_labels = identify_phenotypes(
-        # AT. Some variables are not used anymore, so no point in assigning them: is_label, markers_rep_batches, markers_rep_all
-        # is_label, cell_groups_name, clustering_labels, markers_rep_batches, markers_rep_all = identify_phenotypes(
-                  mat=mat,
-                  markers=markers,
-                  batches=batches,
-                  samples=samples,
-                  is_label=is_label,
-                  max_markers=max_markers,
-                  min_annotations=min_annotations,
-                  min_cellxsample=min_cellxsample,
-                  min_samplesxbatch=min_samplesxbatch,
-                  cell_types_dict=cell_types_dict,
-                  knn_refine=knn_refine,
-                  knn_min_probability=knn_min_probability,
-                  cell_name=label)
+            mat=mat,
+            batches=batches,
+            samples=samples,
+            markers=markers,
+            is_label=is_label,
+            cell_types_dict=cell_types_dict,
+            three_peak_markers=three_peak_markers,
+            # three_peak_markers=['CD4'],  # AT. Remove in prod
+            cell_name=label,
+            max_markers=max_markers,
+            min_annotations=min_annotations,
+            min_cellxsample=min_cellxsample,
+            min_samplesxbatch=min_samplesxbatch,
+            knn_refine=knn_refine,
+            knn_min_probability=knn_min_probability)
 
         cell_dict = dict([tuple([x, cell_groups_name[x].split(" (")[0]])
                           for x in cell_groups_name])
@@ -191,9 +178,20 @@ args = parser.parse_args()
         annotations[is_label] = np.vectorize(cell_dict.get)(clustering_labels)
 
 
+
+
+    # Parse markers as well or ask for a specific file?
+
+
+
+    # # Create output directory if it doesn't exist
+    # output_dir = os.path.dirname(args.output_path)
+    # os.makedirs(output_dir, exist_ok=True)
+
+
+    # adt_cell_type = cellxhaustive()
+    # with open(args.output_path, 'w') as output:
+    # Or pd.to_csv/pd.to_table
+    # Check if I need to create the output dir when using pd.to_csv()
+
 # AT. What is the output format? tsv, table, object... ???
-
-# AT. Copy everything from identify_phenotypes.py here?
-
-# if __name__ == '__main__':
-#     cellxhaustive()
