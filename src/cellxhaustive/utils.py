86664425bbc33b07f723c50eaf7206eb5c93ce4a
aathbt <antonin.thiebaut57@gmail.com>
2024-02-01 15:59:02 +0100

    Adds color formatting to log stream

--- src/cellxhaustive/utils.py
+++ src/cellxhaustive/utils.py
@@ -8,6 +8,32 @@ import logging
 import sys
 
 
+# Custom logging format
+class CustomFormatter(logging.Formatter):
+
+    # Colors and format
+    grey = "\x1b[38;20m"
+    green = "\x1b[32;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[1;31;20m"
+    reset = "\x1b[0m"
+    format = "[%(asctime)s]  %(levelname)-8s  %(message)s"
+    datefmt = '%Y.%m.%d - %H:%M:%S'
+
+    FORMATS = {logging.DEBUG: grey + format + reset,
+               logging.INFO: green + format + reset,
+               logging.WARNING: yellow + format + reset,
+               logging.ERROR: red + format + reset,
+               logging.CRITICAL: bold_red + format + reset}
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt,
+                                      datefmt='%Y.%m.%d - %H:%M:%S')
+        return formatter.format(record)
+
+
 # Function to configurate logging
 def setup_log(logfile, loglevel):
 
@@ -21,11 +47,20 @@ def setup_log(logfile, loglevel):
     else:
         level = logging.WARNING
 
+    # Sets handlers with proper redirection and format
+    # File handler
+    fh_formatter = logging.Formatter(fmt='[%(asctime)s]  %(levelname)-8s  %(message)s',
+                                     datefmt='%Y.%m.%d - %H:%M:%S')
+    handler_fh = logging.FileHandler(logfile, mode='w')
+    handler_fh.setFormatter(fh_formatter)
+    # Stream handler
+    handler_sh = logging.StreamHandler(sys.stdout)
+    handler_sh.setFormatter(CustomFormatter())
+    handlers = [handler_fh,  # Output logging in file
+                handler_sh]  # Output logging in stdout
+
     # Set up logger level, format and handlers
     logging.basicConfig(
         level=level,
-        format="[%(asctime)s]  %(levelname)-8s  %(message)s",
-        datefmt='%Y.%m.%d - %H:%M:%S',
-        handlers=[logging.FileHandler(logfile, mode='w'),  # Output logging in file
-                  logging.StreamHandler(sys.stdout)]  # Output logging in stdout
+        handlers=handlers
     )
