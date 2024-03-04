1420a343a45c30db31c9ab291ebe90e77283ab61
aathbt <antonin.thiebaut57@gmail.com>
2024-03-04 16:11:44 +0100

    Implements parallelisation in score_marker_combinations.py

--- src/cellxhaustive/score_marker_combinations.py
+++ src/cellxhaustive/score_marker_combinations.py
@@ -1,5 +1,5 @@
 """
-Function that determines number of unique cell phenotypes (combination of positive
+Script that determines number of unique cell phenotypes (combination of positive
 and negative markers) and number of cells without phenotype in an expression matrix
 across different metrics thresholds.
 """
@@ -8,6 +8,8 @@ across different metrics thresholds.
 # Import utility modules
 import logging
 import numpy as np
+from multiprocessing import Pool
+from functools import partial
 
 
 # Import local functions
@@ -19,6 +21,86 @@ from determine_marker_status import determine_marker_status  # AT. Double-check 
 # Function used in score_marker_combinations()
 def append_wrapper(lst, elt):
     return lst.append(elt)
+
+
+# Function used in score_marker_combinations()
+def keep_phenotype(batches_label, batch, samples_label, phntp_per_cell, phntp,
+                   x_samplesxbatch_space, y_cellxsample_space):
+    """
+    Function that determines whether a phenotype is relevant, depending on
+    samplesxbatch and cellxsample thresholds.
+
+    Parameters:
+    -----------
+    batches_label: array(str)
+      1-D numpy array with batch names of each cell of 'mat_comb'.
+
+    batch: str
+      Value of batch to process.
+
+    samples_label: array(str)
+      1-D numpy array with sample names of each cell of 'mat_comb'.
+
+    phntp_per_cell: array(str)
+      1-D numpy array with best phenotype determined for each cell.
+
+    phntp: str
+      Value of phenotype to process.
+
+    x_samplesxbatch_space: array(float) (default=[0.5, 0.6, 0.7, ..., 1])
+      Minimum proportion of samples within each batch with at least
+      'y_cellxsample_space' cells for a new annotation to be considered. In other
+      words, by default, an annotation needs to be assigned to at least 10, 20...
+      100 cells/sample (see description of next parameter) in at least 50%, 60%...
+      100% of samples within a batch to be considered.
+
+    y_cellxsample_space: array(float) (default=[10, 20, 30, ..., 100])
+      Minimum number of cells within each sample in 'x_samplesxbatch_space' % of
+      samples within each batch for a new annotation to be considered. In other
+      words, by default, an annotation needs to be assigned to at least 10, 20...
+      100 cells/sample in at least 50%, 60%... 100% of samples (see description
+      of previous parameter) within a batch to be considered.
+
+    Returns:
+    --------
+    keep_phntp_batch: array(bool)
+      2-D numpy array with booleans indicating whether 'phenotype' is relevant
+      in 'batch' across grid composed of metrics 'x_samplesxbatch_space' in D0
+      and 'y_cellxsample_space' in D1.
+    """
+
+    # Split cell type data according to batch
+    phntp_batch = phntp_per_cell[batches_label == batch]
+
+    # Split sample data, first according to batch and then cell type
+    phntp_smpl = samples_label[batches_label == batch][phntp_batch == phntp]
+
+    # Calculate number of different samples in current batch and cell type
+    smpl_nb = float(len(np.unique(phntp_smpl)))
+
+    # Count number of cells per phntp in each sample
+    cell_count_smpl = np.asarray([np.sum(phntp_smpl == smpl)
+                                  for smpl in np.unique(phntp_smpl)])
+
+    # Check whether cell counts satisfy y threshold
+    keep_phntp_batch = (cell_count_smpl[:, np.newaxis] >= y_cellxsample_space)
+    # Note: np.newaxis is used to add a dimension to work on different
+    # samples concurrently
+
+    # Calculate proportion of samples in current batch satisfying
+    # 'y_cellxsample_space' condition
+    keep_phntp_batch = (np.sum(keep_phntp_batch, axis=0) / smpl_nb)
+    # Notes:
+    # - 'keep_phntp_batch' is a boolean array, so it can be summed
+    # - 'np.sum(keep_phntp_batch, axis=0)' calculates number of samples
+    # satisfying y condition for a given y in grid
+
+    # Check whether proportion satisfy x threshold
+    keep_phntp_batch = (keep_phntp_batch >= x_samplesxbatch_space[:, np.newaxis])
+    # Note: '[:, np.newaxis]' is used to transpose 1-D array into a 2-D
+    # array to allow comparison
+
+    return keep_phntp_batch
 
 
 # Function used in check_all_combinations.py
@@ -129,58 +211,31 @@ def score_marker_combinations(mat_comb, batches_label, samples_label,
     # Vectorized version of append wrapper
     array_appending = np.vectorize(append_wrapper, otypes=[str])
 
+    # Get array of unique batches
+    uniq_batches = np.unique(batches_label)
+
     # Process marker phenotypes returned by 'determine_marker_status()' and
     # check whether they are worth keeping
     logging.debug(f'\t\t\t\t\t\tChecking which phenotypes are passing thresholds')
     for phenotype in np.unique(phntp_per_cell):
-        # AT. Multithread/process here? Conflict between batches?
         # Initialise temporary array to store 'phenotype' results
         keep_phenotype = np.full(nb_phntp.shape, True)
 
-        # Process batches separately
-        for batch in np.unique(batches_label):
-            # AT. Multithread/process here?
-            # Split cell type data according to batch
-            phenotypes_batch = phntp_per_cell[batches_label == batch]
+        # Process batches using multiprocessing
+        with Pool() as pool:  # AT. CPU param?
+            keep_phntp_lst = pool.map(partial(keep_phenotype,
+                                              batches_label=batches_label,
+                                              samples_label=samples_label,
+                                              phntp_per_cell=phntp_per_cell,
+                                              phntp=phenotype,
+                                              x_samplesxbatch_space=x_samplesxbatch_space,
+                                              y_cellxsample_space=y_cellxsample_space),
+                                      uniq_batches)
 
-            # Split sample data, first according to batch and then cell type
-            phenotype_samples = samples_label[batches_label == batch][phenotypes_batch == phenotype]
-
-            # If there are no cells of type 'phenotype' in 'batch', that means
-            # 'phenotype' will not be present in all batches, so stop now
-            if phenotype_samples.size == 0:
-                keep_phenotype = np.logical_and(keep_phenotype, False)
-                break
-
-            # Calculate number of different samples in current batch and cell type
-            samples_nb = float(len(np.unique(phenotype_samples)))
-
-            # Count number of cells per phenotype in each sample
-            cell_count_sample = np.asarray([np.sum(phenotype_samples == smpl)
-                                            for smpl in np.unique(phenotype_samples)])
-
-            # Check whether cell counts satisfy y threshold
-            keep_phenotype_batch = cell_count_sample[:, np.newaxis] >= y_cellxsample_space
-            # Note: np.newaxis is used to add a dimension to work on different
-            # samples concurrently
-
-            # Calculate proportion of samples in current batch satisfying
-            # 'y_cellxsample_space' condition
-            keep_phenotype_batch = (np.sum(keep_phenotype_batch, axis=0) / samples_nb)
-            # Notes:
-            # - 'keep_phenotype_batch' is a boolean array, so it can be summed
-            # - 'np.sum(keep_phenotype_batch, axis=0)' calculates number of samples
-            # satisfying y condition for a given y in grid
-
-            # Check whether sample proportions satisfy x threshold
-            keep_phenotype_batch = keep_phenotype_batch >= x_samplesxbatch_space[:, np.newaxis]
-            # Note: '[:, np.newaxis]' is used to transpose 1-D array into a 2-D
-            # array to allow comparison
-
-            # Intersect batch results with general results
-            keep_phenotype = np.logical_and(keep_phenotype, keep_phenotype_batch)
-            # Note: for consistency, phenotypes have to be present in all batches,
-            # hence usage of 'np.logical_and()'
+        # Intersect all batch results to retain phenotypes present in all
+        keep_phenotype = np.logical_and.reduce(keep_phntp_lst)
+        # Note: for consistency, phntps have to be present in all batches,
+        # hence usage of 'np.logical_and()'
 
         # Add 'phenotype' presence/absence to cell type counter
         nb_phntp += keep_phenotype * 1
