11888b4c315192b055855093bae2e0b035221d8c
aathbt <antonin.thiebaut57@gmail.com>
2024-03-06 15:49:58 +0100

    Implements parallelisation in check_all_combinations.py

--- src/cellxhaustive/check_all_combinations.py
+++ src/cellxhaustive/check_all_combinations.py
@@ -10,11 +10,14 @@ phenotypes and minimizing number of cells without phenotypes.
 import itertools as ite
 import logging
 import numpy as np
+from functools import partial
 
 
 # Import local functions
 from score_marker_combinations import score_marker_combinations  # AT. Double-check path
+from utils import NestablePool  # AT. Double-check path
 # from cellxhaustive.score_marker_combinations import score_marker_combinations
+# from cellxhaustive.utils import NestablePool
 
 
 # Convenience function to return specific dictionary values
@@ -23,6 +26,158 @@ def return_outputs(dict1, dict2, dict3, idx1, idx2):
     out2 = dict2[idx1[idx2[0]]]
     out3 = np.concatenate(dict3[idx1[idx2[0]]])
     return out1, out2, out3
+
+
+# Function used in check_all_combinations()
+def evaluate_comb(idx, comb, mat_representative, batches_label, samples_label,
+                  markers_representative, two_peak_threshold, three_peak_markers,
+                  three_peak_low, three_peak_high, min_annotations,
+                  x_samplesxbatch_space, y_cellxsample_space):
+    """
+    Function that scores a marker combination and checks whether it contains
+    relevant solutions depending on number of phenotypes detected, proportion of
+    samples within batch displaying the phenotypes, number of cells within each
+    sample displaying the phenotypes and number of cells without phenotypes.
+
+    Parameters:
+    -----------
+    idx: int
+      Integer index to keep track of 'comb'.
+
+    comb: tuple(str)
+      Tuple of strings marker combination to score.
+
+    mat_representative: array(float)
+      2-D numpy array expression matrix, with cells in D0 and markers in D1.
+      In other words, rows contain cells and columns contain markers. This
+      matrix is a subset of the general expression matrix and contains sliced
+      data matching cell label, batch, and representative markers.
+
+    batches_label: array(str)
+      1-D numpy array with batch names of each cell of 'mat_representative'.
+
+    samples_label: array(str)
+      1-D numpy array with sample names of each cell of 'mat_representative'.
+
+    markers_representative: array(str)
+      1-D numpy array with markers matching each column of 'mat_representative'.
+
+    two_peak_threshold: float (default=3)
+      Threshold to consider when determining whether a two-peaks marker is
+      negative or positive. Expression below this threshold means marker will be
+      considered negative. Conversely, expression above this threshold means
+      marker will be considered positive.
+
+    three_peak_markers: list(str) (default=[])
+      List of markers that have three peaks.
+
+    three_peak_low: float (default=2)
+      Threshold to consider when determining whether a three-peaks marker is
+      negative or low positive. Expression below this threshold means marker will
+      be considered negative. See description of 'three_peak_high' for
+      more information on low_positive markers.
+
+    three_peak_high: float (default=4)
+      Threshold to consider when determining whether a three-peaks marker is
+      low_positive or positive. Expression above this threshold means marker will
+      be considered positive. Expression between 'three_peak_low' and
+      'three_peak_high' means marker will be considered low_positive.
+
+    min_annotations: int (default=3)
+      Minimum number of phenotypes for a combination of markers to be taken into
+      account as a potential cell population. Must be in '[2; len(markers)]',
+      but it is advised to choose a value in '[3; len(markers) - 1]'.
+
+    x_samplesxbatch_space: array(float) (default=[0.5, 0.6, 0.7, ..., 1])
+      Minimum proportion of samples within each batch with at least
+      'y_cellxsample_space' cells for a new annotation to be considered. In other
+      words, by default, an annotation needs to be assigned to at least 10, 20...
+      100 cells/sample (see description of next parameter) in at least 50%, 60%...
+      100% of samples within a batch to be considered.
+
+    y_cellxsample_space: array(float) (default=[10, 20, 30, ..., 100])
+      Minimum number of cells within each sample in 'x_samplesxbatch_space' % of
+      samples within each batch for a new annotation to be considered. In other
+      words, by default, an annotation needs to be assigned to at least 10, 20...
+      100 cells/sample in at least 50%, 60%... 100% of samples (see description
+      of previous parameter) within a batch to be considered.
+
+    Returns:
+    --------
+    comb_result_dict: dict {str: obj}
+      Dictionary with 1 or 8 key-value pairs. If no relevant solution was found,
+      dictionary will have following structure {idx: None}. If relevant solution
+      was found, keys will be 'idx', 'comb', 'phntp_per_cell', 'max_nb_phntp',
+      'min_undefined', 'max_x_values', 'max_y_values', and 'best_phntp_lst'
+    """
+
+    logging.debug(f'\t\t\t\tTesting {comb}')
+    # Slice data based on current marker combination 'comb'
+    markers_comb = markers_representative[np.isin(markers_representative, np.asarray(comb))]
+    mat_comb = mat_representative[:, np.isin(markers_representative, markers_comb)]
+
+    # Find number of phenotypes and undefined cells for a given marker combination
+    # 'comb' across 'samplesxbatch' and 'cellxsample' grid
+    logging.debug(f'\t\t\t\t\tScoring combination')
+    nb_phntp, phntp_to_keep, nb_undef_cells, phntp_per_cell = score_marker_combinations(  # AT. CPU param?
+        mat_comb=mat_comb,
+        batches_label=batches_label,
+        samples_label=samples_label,
+        markers_comb=markers_comb,
+        two_peak_threshold=two_peak_threshold,
+        three_peak_markers=three_peak_markers,
+        three_peak_low=three_peak_low,
+        three_peak_high=three_peak_high,
+        x_samplesxbatch_space=x_samplesxbatch_space,
+        y_cellxsample_space=y_cellxsample_space)
+
+    # Constrain matrix given minimum number of phenotype conditions
+    mask = (nb_phntp < min_annotations)
+    nb_phntp = np.where(mask, np.nan, nb_phntp)
+    nb_undef_cells = np.where(mask, np.nan, nb_undef_cells)
+
+    # If there are possible good solutions, further process them
+    logging.debug(f'\t\t\t\t\tChecking presence of good solutions')
+    if np.any(np.isfinite(nb_phntp)):
+        # Create metrics grid matching x and y space
+        x_values, y_values = np.meshgrid(x_samplesxbatch_space,
+                                         y_cellxsample_space,
+                                         indexing='ij')
+
+        # Constrain grid
+        x_values = np.where(mask, np.nan, x_values)
+        y_values = np.where(mask, np.nan, y_values)
+
+        # Best solution has maximum number of new phenotypes...
+        max_nb_phntp = np.nanmax(nb_phntp)
+        nb_undef_cells[nb_phntp != max_nb_phntp] = np.nan
+
+        # ... and minimum number of undefined cells...
+        min_undefined = np.nanmin(nb_undef_cells)
+        x_values[nb_undef_cells != min_undefined] = np.nan
+
+        # ... and maximum percentage within batch
+        max_x_values = np.nanmax(x_values)
+        y_values[x_values != max_x_values] = np.nan
+
+        # ... and maximum cells per sample
+        max_y_values = np.nanmax(y_values)
+        best_phntp_lst = phntp_to_keep[y_values == max_y_values]
+
+        # Gather all results in dict
+        comb_result_dict = {'idx': idx,
+                            'comb': comb,
+                            'phntp_per_cell': phntp_per_cell,
+                            'max_nb_phntp': max_nb_phntp,
+                            'min_undefined': min_undefined,
+                            'max_x_values': max_x_values,
+                            'max_y_values': max_y_values,
+                            'best_phntp_lst': best_phntp_lst}
+
+    else:  # No good solution, so return None to facilitate post-processing
+        comb_result_dict = {idx: None}
+
+    return comb_result_dict
 
 
 # Function used in identify_phenotypes.py
@@ -103,10 +258,10 @@ def check_all_combinations(mat_representative, batches_label, samples_label,
       Number of optimal combinations found when checking and comparing all possible
       marker combinations.
 
-    best_marker_comb: tuple or list(tuple)
-      Tuple or list of tuples with optimal combinations found during comparison
-      process. Each tuple contains one combination. Number of tuples in
-      'best_marker_comb' is equal to 'nb_solution'.
+    best_marker_comb: tuple(str) or list(tuple(str))
+      Tuple of strings or list of tuples of strings with optimal combinations
+      found during comparison process. Each tuple contains one combination.
+      Number of tuples in 'best_marker_comb' is equal to 'nb_solution'.
 
     cell_phntp_comb: array(str) or list(array(str))
       1-D numpy array of strings or list of 1-D numpy arrays of strings with phenotype
@@ -131,13 +286,13 @@ def check_all_combinations(mat_representative, batches_label, samples_label,
     # Initialise counters and objects to store results. Note that by default, it
     # is assumed that minimum number of relevant markers is 2 (only 1 marker can
     # not define a phenotype)
+    enum_start = 0
     marker_counter = 2
-    comb_idx = 0
+    max_nb_phntp_marker = 0
+    max_nb_phntp_tot = 0
     comb_dict = {}
     cell_phntp_dict = {}
     phntp_list_dict = {}
-    max_nb_phntp_marker = 0
-    max_nb_phntp_tot = 0
 
     # Empty arrays to store results and find best marker combinations
     best_comb_idx = np.empty(0)
@@ -154,7 +309,7 @@ def check_all_combinations(mat_representative, batches_label, samples_label,
     while ((marker_counter <= max_combination)
            and (max_nb_phntp_tot <= max_nb_phntp_marker)):
 
-        # If max number of phenotypes does not increase and is not nul, stop now.
+        # If max number of phenotypes does not increase and is not null, stop now.
         # This avoids both a 'plateau effect' (increase number of markers without
         # improving number of phenotypes) and finding no phenotype
         if (max_nb_phntp_tot == max_nb_phntp_marker) and (max_nb_phntp_tot != 0):
@@ -164,98 +319,68 @@ def check_all_combinations(mat_representative, batches_label, samples_label,
         # Save new higher (or equal) maximum number of phenotypes
         max_nb_phntp_tot = max_nb_phntp_marker
 
-        # Re-initialise counter of maximum phenotypes for combinations with
-        # 'marker_counter' elements
-        max_nb_phntp_marker = 0
+        # Get all possible combinations containing 'marker_counter' markers
+        poss_comb = ite.combinations(markers_representative, marker_counter)
 
-        # For a given number of markers, check all possible marker combinations
-        for comb in ite.combinations(markers_representative, marker_counter):
-            logging.debug(f'\t\t\t\tTesting {comb}')
-            # AT. Opportunity to multiprocess? Or not because we need to test 2
-            # markers first, then 3, then 4... And stop if nothing better is found
-            # Slice data based on current marker combination 'comb'
-            markers_comb = markers_representative[np.isin(markers_representative, np.asarray(comb))]
-            mat_comb = mat_representative[:, np.isin(markers_representative, markers_comb)]
+        # For a given number of markers, check all possible combinations using multiprocessing
+        with NestablePool() as pool:  # AT. CPU param?
+            score_results_lst = pool.starmap(partial(evaluate_comb,
+                                                     mat_representative=mat_representative,
+                                                     batches_label=batches_label,
+                                                     samples_label=samples_label,
+                                                     markers_representative=markers_representative,
+                                                     two_peak_threshold=two_peak_threshold,
+                                                     three_peak_markers=three_peak_markers,
+                                                     three_peak_low=three_peak_low,
+                                                     three_peak_high=three_peak_high,
+                                                     min_annotations=min_annotations,
+                                                     x_samplesxbatch_space=x_samplesxbatch_space,
+                                                     y_cellxsample_space=y_cellxsample_space),
+                                             enumerate(poss_comb, enum_start))
 
-            # Find number of phenotypes and undefined cells for a given marker
-            # combination 'comb' across 'samplesxbatch' and 'cellxsample' grid
-            logging.debug(f'\t\t\t\t\tScoring combination')
-            nb_phntp, phntp_to_keep, nb_undef_cells, phntp_per_cell = score_marker_combinations(
-                mat_comb=mat_comb,
-                batches_label=batches_label,
-                samples_label=samples_label,
-                markers_comb=markers_comb,
-                two_peak_threshold=two_peak_threshold,
-                three_peak_markers=three_peak_markers,
-                three_peak_low=three_peak_low,
-                three_peak_high=three_peak_high,
-                x_samplesxbatch_space=x_samplesxbatch_space,
-                y_cellxsample_space=y_cellxsample_space)
+        # Remove combinations without solution and turn list into dict using idx as keys
+        score_results_dict = {}
+        for dct in score_results_lst:
+            if len(dct) > 1:
+                idx = dct.pop('idx')
+                score_results_dict[idx] = dct
 
-            # Constrain matrix given minimum number of phenotype conditions
-            mask = (nb_phntp < min_annotations)
-            nb_phntp = np.where(mask, np.nan, nb_phntp)
-            nb_undef_cells = np.where(mask, np.nan, nb_undef_cells)
-
-            # If there are possible good solutions, further process them
-            logging.debug(f'\t\t\t\t\tChecking presence of good solutions')
-            if np.any(np.isfinite(nb_phntp)):
-                # Calculate maximum number of phenotypes for marker combination
-                # 'comb'. This counter is useful for two things. 1. Determine
-                # maximum number of phenotypes found across all combinations with
-                # 'marker_counter' elements. 2. Avoid processing and keeping
-                # 'comb' with a poor score: if 'comb' max is worse than recorded
-                # best overall (meaning all 'comb' previously analysed), it
-                # isn't worth keeping
-                max_nb_phntp_comb = np.nanmax(nb_phntp)
-                if ((max_nb_phntp_comb >= max_nb_phntp_marker)
-                        and (max_nb_phntp_comb >= max_nb_phntp_tot)):
-                    max_nb_phntp_marker = max_nb_phntp_comb
-                else:
-                    logging.debug('\t\t\t\t\t\tNo good solution, skipping to next combination')
-                    continue
-
-                logging.debug(f'\t\t\t\t\tProcessing and storing results')
-                # If there are interesting phenotypes, store marker combination
-                # as well as phenotype per cell
-                comb_dict[comb_idx] = comb
-                cell_phntp_dict[comb_idx] = phntp_per_cell
-
-                # Create metrics grid matching x and y space
-                x_values, y_values = np.meshgrid(x_samplesxbatch_space,
-                                                 y_cellxsample_space,
-                                                 indexing='ij')
-
-                # Constrain grid
-                x_values = np.where(mask, np.nan, x_values)
-                y_values = np.where(mask, np.nan, y_values)
-
-                # Calculate minimum number of undefined cells for 'comb'
-                min_nb_undefined = np.nanmin(nb_undef_cells)
-
-                # Best solution has maximum number of new phenotypes...
-                best_comb_idx = np.append(best_comb_idx, comb_idx)  # Index of comb to use in 'comb_dict'
-                best_nb_phntp = np.append(best_nb_phntp, max_nb_phntp_comb)
-
-                # ... and minimum number of undefined cells
-                nb_undef_cells[nb_phntp != max_nb_phntp_comb] = np.nan
-                best_nb_undefined = np.append(best_nb_undefined, min_nb_undefined)
-
-                # ... and maximum percentage within batch
-                x_values[nb_undef_cells != min_nb_undefined] = np.nan
-                best_x_values = np.append(best_x_values, np.nanmax(x_values))
-
-                # ... and maximum cells per sample
-                y_values[x_values != np.nanmax(x_values)] = np.nan
-                best_y_values = np.append(best_y_values, np.nanmax(y_values))
-
-                # Keep best phenotypes list
-                best_phntp_lst = phntp_to_keep[y_values == np.nanmax(y_values)]
-                phntp_list_dict[comb_idx] = best_phntp_lst
-
-            comb_idx += 1
-
+        # Increase marker counter; it doesn't matter whether a solution is found
         marker_counter += 1
+
+        # Reset enumerate start to avoid overwriting data in next iteration
+        enum_start = len(score_results_lst)
+
+        # Post-process results
+        if len(score_results_dict) == 0:  # No combination is relevant, skip to next iteration
+            max_nb_phntp_marker = 0  # Re-initialise counter of maximum phenotype
+            continue
+        else:  # One or more combination are relevant
+            # Filter out combinations not reaching maximum number of phenotype
+            max_nb_phntp_marker = max(dct['max_nb_phntp'] for dct in score_results_dict.values())
+            score_max_phntp = {k: v for k, v in score_results_dict.items() if v['max_nb_phntp'] == max_nb_phntp_marker}
+
+            # Filter out combinations not reaching minimum number of undefined cells
+            min_nb_undef = max(dct['min_undefined'] for dct in score_max_phntp.values())
+            score_min_undef = {k: v for k, v in score_max_phntp.items() if v['min_undefined'] == min_nb_undef}
+
+            # Filter out combinations not reaching maximum samplesxbatch
+            max_x_val = max(dct['max_x_values'] for dct in score_min_undef.values())
+            score_max_x = {k: v for k, v in score_min_undef.items() if v['max_x_values'] == max_x_val}
+
+            # Filter out combinations not reaching maximum cellxsample
+            max_y_val = max(dct['max_y_values'] for dct in score_max_x.values())
+            score_final = {k: v for k, v in score_max_x.items() if v['max_y_values'] == max_y_val}
+
+            # Save best results in general dictionaries and arrays
+            comb_dict |= {k: v['comb'] for k, v in score_final.items()}
+            cell_phntp_dict |= {k: v['phntp_per_cell'] for k, v in score_final.items()}
+            phntp_list_dict |= {k: v['best_phntp_lst'] for k, v in score_final.items()}
+            best_comb_idx = np.append(best_comb_idx, np.fromiter((score_final.keys()), dtype=int))
+            best_nb_phntp = np.append(best_nb_phntp, np.fromiter((d['max_nb_phntp'] for d in score_final.values()), dtype=float))
+            best_nb_undefined = np.append(best_nb_undefined, np.fromiter((d['min_undefined'] for d in score_final.values()), dtype=float))
+            best_x_values = np.append(best_x_values, np.fromiter((d['max_x_values'] for d in score_final.values()), dtype=float))
+            best_y_values = np.append(best_y_values, np.fromiter((d['max_y_values'] for d in score_final.values()), dtype=float))
 
     # If no marker combination was found, stop now
     if len(best_nb_phntp) == 0:
